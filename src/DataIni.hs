70bc3e18a28729f78e528bd08f326d1b03747d9c
Василий Симаков <vasiliy@sim.papa>
2018-01-15 00:28:58 +0300

    Finished parse Ini files

--- src/DataIni.hs
+++ src/DataIni.hs
@@ -17,8 +17,50 @@ import Text.Trifecta
 headerEx :: ByteString
 headerEx = "[blah]"
 -- "[blah]" -> Section "blah"
-
 newtype Header = Header String deriving (Eq, Ord, Show)
+
+assingmentEx :: ByteString
+assingmentEx = "woot=1"
+
+skipComment :: ByteString 
+skipComment = "; woot\n[blah]"
+
+
+commentEx :: ByteString
+commentEx = "; last modified 1 April\
+              \ 2001 by John Doe"
+
+commentEx' :: ByteString
+commentEx' = "; blah\n; woot\n \n;hah"
+
+sectionEx :: ByteString
+sectionEx = "; ignore me\n[states]\nChris=Texas"
+
+sectionEx' :: ByteString
+sectionEx' = [r|
+; ignore me
+[states]
+Chris=Texas
+|]
+
+sectionEx'' :: ByteString
+sectionEx'' = [r|
+; comment
+[section]
+host=wikipedia.org
+alias=claw
+
+[whatisit]
+red=intoothandclaw
+|]
+
+type Name = String
+type Value = String
+type Assingments = Map Name Value
+
+data Section = Section Header Assingments deriving (Eq,Show)
+
+newtype Config = Config (Map Header Assingments) deriving (Eq,Show)
 
 parseBracketPair :: Parser a -> Parser a
 parseBracketPair p = char '[' *> p <* char ']'
@@ -29,13 +71,6 @@ parseBracketPair p = char '[' *> p <* char ']'
 
 parseHeader :: Parser Header
 parseHeader = parseBracketPair (Header <$> some letter)
-
-assingmentEx :: ByteString
-assingmentEx = "woot=1"
-
-type Name = String
-type Value = String
-type Assingments = Map Name Value
 
 parseAssignment :: Parser (Name, Value)
 parseAssignment = do
@@ -48,3 +83,35 @@ parseAssignment = do
 -- | Skip end of line and whitespace beyond.
 skipEOL :: Parser ()
 skipEOL = skipMany (oneOf "\n")
+
+skipComments :: Parser ()
+skipComments =
+  skipMany (do _ <- char ';' <|> char '#'
+               skipMany (noneOf "\n")
+               skipEOL )
+
+skipWhitespace :: Parser ()
+skipWhitespace = skipMany (char ' ' <|> char '\n')
+
+parseSection :: Parser Section
+parseSection = do
+  skipWhitespace
+  skipComments
+  h <- parseHeader
+  skipEOL
+  assingments <- some parseAssignment
+  return $ Section h (M.fromList assingments)
+
+rollup :: Section -> Map Header Assingments -> Map Header Assingments
+rollup (Section h a) m = M.insert h a m
+
+parseIni :: Parser Config
+parseIni = do
+  sections <- some parseSection
+  let mapOfSection = foldr rollup M.empty sections
+  return (Config mapOfSection)
+
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _           = Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,52 @@
+import DataIni
+import Test.Hspec
+import Data.ByteString (ByteString)
+import Text.Trifecta
+import Data.Map (Map)
+import qualified Data.Map as M
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+--main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+
+  describe "Assingment Parsing" $ --do
+    it "can parse a simple assingment" $ do
+      let m = parseByteString parseAssignment mempty assingmentEx
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just("woot","1")
+
+  describe "Header Parsing" $ --do
+    it "can parse a simple header" $ do
+      let m = parseByteString parseHeader mempty headerEx
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just(Header "blah")
+
+  describe "Comment Parsing" $ --do
+    it "Can skip a comment before a header" $ do
+      let p = skipComments >> parseHeader
+          --i = "; woot\n[blah]"
+          m = parseByteString p mempty skipComment
+          r' = maybeSuccess m
+      print m
+      r' `shouldBe` Just(Header "blah")
+
+  describe "Section Parsing" $
+    it "Can parse simple section" $ do
+      let m = parseByteString parseSection mempty sectionEx
+          r' = maybeSuccess m
+          states = M.fromList [("Chris","Texas")]
+          expected' = Just (Section (Header "states") states)
+      print m
+      r' `shouldBe` expected'
+
+  describe "INI parsing" $
+    it "Can parse multiple sections" $ do
+      let m = parseByteString parseIni mempty sectionEx''
+          r' = maybeSuccess m
+          sectionValues = M.fromList [("alias","claw"),("host","wikipedia.org")]
+          whatIsItValues = M.fromList [("red","intoothandclaw")]
+          expected' = Just (Config (M.fromList [(Header "section", sectionValues),(Header "whatisit", whatIsItValues)]))
+      print m
+      r' `shouldBe` expected'
